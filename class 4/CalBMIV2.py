8515322c920cc6b836fd5526ff01cdbcfa9b7993
fangjiaqi <370358679@qq.com>
2018-05-29 19:49:22 +0800

    计算BMI

---
+++ class 4/CalBMIV2.py
@@ -0,0 +1,16 @@
+#CalBMIV2.py
+
+height,weight = eval(input("请输入身高（米）和体重（公斤）[逗号隔开]："))
+bmi = weight / pow(height, 2)
+print("BMI数值为：{:.2f}".format(bmi))
+who = ""
+if bmi < 18.5:
+    who = "偏瘦"
+elif 18.5 <= bmi < 24:
+    who = "正常"
+elif 24 <= bmi < 28:
+    who = "偏胖"
+else:
+    who = "肥胖"
+print("BMI 指标为：国内'{0}'".format(who))
+
