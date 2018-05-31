6f736032ffd70c8a1986c5daff713a67ce047120
fjq <370358679@qq.com>
2018-05-31 16:47:58 +0800

    政府工作报告词云

--- basic/week7/wordcloud-ch.py
+++ basic/week7/wordcloud-ch.py
@@ -1,7 +1,8 @@
 import jieba
 import wordcloud
 txt = "程序设计语言是计算机能够理解和识别用户操作意图的一种交互体系，它按照特定规则组织计算机指令，使计算机能够自动进行各种运算处理。"
-w = wordcloud.WordCloud(width=1000,height=700,font_path="msyh.ttc")
-w.generate(" ",join(jieba.lcut(txt)))
+w = wordcloud.WordCloud(width=1000, height=700, font_path="/Library/Fonts/Arial Unicode.ttf")
+t = " ".join(jieba.lcut(txt))
+w.generate(t)
 w.to_file("pywcloud.png")
 
