f337d7e9ff34eeee2c7de942364b1b82160cc672
fjq <370358679@qq.com>
2018-05-31 17:24:56 +0800

    增加图形

--- basic/week7/GovRptWordCloudV2.py
+++ basic/week7/GovRptWordCloudV2.py
@@ -1,12 +1,13 @@
 #GovRptWordCloudV2.py
 import jieba
 import wordcloud
+from scipy.misc imread
+mask = imread("fivestar.png")
 f = open("关于实施乡村振兴战略的意见.txt", "r", encoding="utf-8")
 t = f.read()
 f.close()
 ls = jieba.lcut(t)
 txt = " ".join(ls)
-w = wordcloud.WordCloud(font_path="/Library/Fonts/Arial Unicode.ttf",
-                        width=1000, height=700, background_color="white", max_words=15)
+w = wordcloud.WordCloud(font_path="/Library/Fonts/Arial Unicode.ttf", mask = mask, width=1000, height=700, background_color="white", max_words=15)
 w.generate(txt)
 w.to_file("grwordcloud.png")
