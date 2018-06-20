5a6d56741b3f794f9444d8a8a6c73567ee461131
fangjiaqi77 <370358679@qq.com>
2018-06-20 15:48:19 +0800

    airline

---
+++ test/minhang_school.py
@@ -0,0 +1,23 @@
+#coding=utf-8
+import requests
+from bs4 import BeautifulSoup
+import traceback
+import re
+
+
+def getHTMLText(url, code="utf-8"):
+    header = {"user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36"}
+    try:
+        r = requests.get(url, headers=header)
+        r.raise_for_status()
+        r.encoding = code
+        return r.text
+    except:
+        return ""
+
+def main():
+    school_list_url = 'http://www.mhedu.sh.cn/school/list.jsp?cur_page=1&tm_id=475&xdfl=19&sdjd=&djjb=&jbzlx=&xxmc='
+    s = getHTMLText(school_list_url)
+    print(s)
+
+main()
