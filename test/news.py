168be390f03ccdcad5b413478eea48c56adc8b0b
fangjiaqi77 <370358679@qq.com>
2018-06-22 11:18:20 +0800

    news

--- test/news.py
+++ test/news.py
@@ -1,4 +1,14 @@
 import requests
+from bs4 import BeautifulSoup
+
+
 res = requests.get('http://news.sina.com.cn/china/')
 res.encoding = 'utf-8'
-print(res.text)
+#print(res.text)
+soup = Beautifulsoup(res, 'html.parser')
+print(soup.text)
+
+
+
+
+
