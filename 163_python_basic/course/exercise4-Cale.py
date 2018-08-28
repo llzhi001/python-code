96b9b0990a523b289151dd66a6ba4cd181858906
fangjiaqi77 <370358679@qq.com>
2018-08-28 11:40:42 +0800

    exercise4

---
+++ 163_python_basic/course/exercise4-Cale.py
@@ -0,0 +1,16 @@
+# CaleV1.py
+x =1024 * 1024
+print(pow(1+1/x, x))
+
+
+#CaleV2.py
+from random import *
+DARTS = 1024 * 1024
+count = 0
+for i in range(DARTS):
+    x = uniform(1,2)
+    y = uniform(0,1)
+    if x*y < 1:
+        count += 1
+e = pow(2,DARTS/count)
+print("{}".format(e))
